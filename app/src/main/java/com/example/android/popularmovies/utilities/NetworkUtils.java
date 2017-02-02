b4bd56ac7d35de6321c9dbd15d6f86f7dc5cf5c5
Lucas Horiye <lucas2612@gmail.com>
2017-02-02 16:41:31 -0200

    NetworkUtils

---
+++ app/src/main/java/com/example/android/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,58 @@
+package com.example.android.popularmovies.utilities;
+
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by lucas on 02/02/2017.
+ */
+
+public class NetworkUtils {
+
+    public final static String URL_MOVIE_BASE = "http://api.themoviedb.org/3/movie/";
+    public final static String API_KEY = "xxxxxxxxx";
+
+    public static URL buildUrl(String order) {
+        Uri builtUri = Uri.parse(URL_MOVIE_BASE).buildUpon()
+                .appendPath(order)
+                .appendQueryParameter("api_key", API_KEY)
+                .build();
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Log.v(TAG, "Built URI " + url);
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
